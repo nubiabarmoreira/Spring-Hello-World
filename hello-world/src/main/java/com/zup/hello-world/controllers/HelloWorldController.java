153d34444dee3d9d905a6718855ba79ffd9b89e4
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-01-14 20:08:41 -0300

    chore: ajusta estrutura do projeto

--- hello-world/src/main/java/com/zup/hello-world/controllers/HelloWorldController.java
+++ hello-world/src/main/java/com/zup/hello-world/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.zup.helloworld;
+package com.zup.hello-world;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
