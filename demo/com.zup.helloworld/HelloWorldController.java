209a76313f6c6e833ff3ad9356a283301225d9c2
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-01-14 19:47:12 -0300

    feat: adiciona controlador que retorna Hello World!

---
+++ demo/com.zup.helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.zup.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
